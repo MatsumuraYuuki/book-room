a9b32fdbf5f6aa0c80da9d02373229b2c0627966
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-08-05 12:23:28 +0900

    Gem:FactoryBotとFakerを追加・設定。ユーザーの

---
+++ rails/spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  context "factoryのデフォルト設定に従った場合" do
+    # factories/users.rbの情報を参照してuserを作成
+    let(:user) { create(:user) }
+
+    it "認証済みの user レコードを正常に新規作成できる" do
+      # user は valid であること（バリデーションエラーがないこと）を検証
+      expect(user).to be_valid
+      # user が認証済み（confirmed_atカラムに値が含まれていること）を検証
+      expect(user).to be_confirmed
+    end
+  end
+end
+
+
+「ログインユーザー取得アクションのリクエストスペック実装」のところまでやった

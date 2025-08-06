d89157b3367637bec0e24e10d1039ed387b4027e
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-08-06 17:42:32 +0900

    記事コントローラーに、index・showアクションとRspecを実装

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::Current::ArticlesController < Api::V1::BaseController
   before_action :authenticate_user!
 
+  def index
+    # .not_unsavedはRailsのenumによる自動生成メソッド
+    articles = current_user.articles.not_unsaved.order(created_at: :desc)
+    render json: articles
+  end
+
+  def show
+    article = current_user.articles.find(params[:id])
+    render json: article
+  end  
+
   def create
     # 「||」左側が falsy（偽値：nil, false）な場合：右側が実行され、その値が返される
     unsaved_article = current_user.articles.unsaved.first || current_user.articles.create!(status: :unsaved)

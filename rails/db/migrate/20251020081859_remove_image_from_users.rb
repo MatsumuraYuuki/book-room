2462aa289a1c443912ae1745d5f6783c4b6d8fa8
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-10-21 14:20:51 +0900

    feat: 不要なnicknameおよびimageカラムをコメントアウトし、テストデータにのプロフィール画像を追加

--- rails/db/migrate/20251020081859_remove_image_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromUsers < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :users, :image, :string
-  end
-end
--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,5 +1,20 @@
 ActiveRecord::Base.transaction do
-  User.create!(name: "テスト太郎", email: "test1@example.com", password: "password", confirmed_at: Time.current)
+  User.create!(
+    name: "テスト太郎",
+    email: "test1@example.com",
+    password: "password",
+    confirmed_at: Time.current
+    ).
+    image.attach(io: File.open(Rails.root.join('app/assets/images/cat.webp')),
+                  filename: 'cat.webp')
 
-  User.create!(name: "テスト次郎", email: "test2@example.com", password: "password", confirmed_at: Time.current)
+
+  User.create!(
+    name: "テスト次郎",
+    email: "test2@example.com",
+    password: "password",
+    confirmed_at: Time.current
+    ).
+    image.attach(io: File.open(Rails.root.join('app/assets/images/dog.webp')),
+                  filename: 'dog.webp')
 end

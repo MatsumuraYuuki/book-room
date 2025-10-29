dbad286829d35bbd74b588bd97fc45cf25cb1cf5
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-10-29 20:21:49 +0900

    test: PATCH /api/v1/current/user のテストを追加

--- rails/app/controllers/api/v1/current/user_controller.rb
+++ rails/app/controllers/api/v1/current/user_controller.rb
@@ -1,6 +1,3 @@
-# currentディレクトリ以下に作成したのは「ログイン中のユーザー自身に関する操作」を明確に分離するための設計
-# 継承元をApplicationController から Api::V1::BaseControllerに変更
-# コントローラーの継承元をApi::V1::BaseControllerにすることで、 devise 提供のメソッドをエイリアス後の命名で使用できる
 class Api::V1::Current::UserController < Api::V1::BaseController
   before_action :authenticate_user! # ユーザーが認証済みであるか否かをbooleanで返す
 

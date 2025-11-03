63cd23472fc55100640efb9a34a6fcb34fb4cfa9
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-11-03 19:30:16 +0900

    feat: AozoraBookモデルのバリデーションを追加、schema.rbの不要なコメントを削除

--- rails/app/models/aozora_book.rb
+++ rails/app/models/aozora_book.rb
@@ -1,4 +1,23 @@
 class AozoraBook < ApplicationRecord
+  has_many :bookshelves, dependent: :destroy
+  has_many :users, through: :bookshelves
+
+  # バリデーション
+  validates :aozora_book_id, 
+    presence: true, 
+    uniqueness: true
+  
+  validates :title, 
+    presence: true
+  
+  validates :author, 
+    presence: true
+  
+  validates :aozora_content_url, 
+    presence: true
+  
+  
+  
   # /api/v1/aozora_books_controller.rbから引数keywordを受け取っている
   def self.search(keyword) 
     return AozoraBook.none if keyword.blank?

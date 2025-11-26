715f3f91188cd63f646207b7e9ca1f1569db9e35
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-11-26 13:48:39 +0900

    fix: BookshelfSerializerにcreated_atを追加

--- rails/app/serializers/bookshelf_serializer.rb
+++ rails/app/serializers/bookshelf_serializer.rb
@@ -1,5 +1,5 @@
 class BookshelfSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :aozora_book_id, :status, :completed_at, :created_at
+  attributes :id, :user_id, :aozora_book_id, :status, :completed_at, :created_at, :updated_at
 
   has_one :aozora_book # 関連するモデル
 end

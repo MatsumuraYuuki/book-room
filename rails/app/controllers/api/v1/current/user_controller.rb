5be5ccd62bfbc25576392c0eccc98e67844b8e52
MatsumuraYuuki <rikkyo.matumura@gmail.com>
2025-11-26 15:58:29 +0900

    fix: バックエンド側で、パスワード更新時のバリデーション追加

--- rails/app/controllers/api/v1/current/user_controller.rb
+++ rails/app/controllers/api/v1/current/user_controller.rb
@@ -7,16 +7,31 @@ class Api::V1::Current::UserController < Api::V1::BaseController
   end
 
   def update
-    if current_user.update(user_params)
+    # パスワード変更がある場合は update_with_password を使用
+    result = if password_change?
+      current_user.update_with_password(user_params_with_password)
+    else
+      current_user.update(user_params)
+    end
+
+    if result
       render json: current_user, serializer: CurrentUserSerializer
     else
-      render json: { error: "User not updated" }, status: :unprocessable_entity
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
+    def password_change?
+      params[:user][:password].present? || params[:user][:password_confirmation].present?
+    end
+
     def user_params
-      params.require(:user).permit(:name, :image)
+      params.require(:user).permit(:name, :email, :image)
+    end
+
+    def user_params_with_password
+      params.require(:user).permit(:name, :email, :image, :current_password, :password, :password_confirmation)
     end
 end
